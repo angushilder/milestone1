c480478f1543b83badfaef43e0f3786f2d80df48
Ubuntu <ubuntu@ip-172-31-24-175.ec2.internal>
2019-09-10 17:34:12 +0000

    Gallery images added

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
-/*background image*/
+/*gallery*/
 
-$(document).ready(function(){
-  $('.header').height($(window).height());
-})
+// MDB Lightbox Init
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
