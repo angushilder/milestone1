26d7e4205068d06b69db0865ca7a9fd34051787b
Ubuntu <ubuntu@ip-172-31-24-175.ec2.internal>
2019-09-11 18:49:40 +0000

    gallery working, with incorrect images at present. need to update

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,14 @@
 /*gallery*/
 
-// MDB Lightbox Init
-$(function () {
-$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
 });
